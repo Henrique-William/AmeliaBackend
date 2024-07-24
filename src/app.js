199a11cc3060a8919f22772d0001b4b53400c5ec
macharetel <macharetelucas@gmail.com>
2024-07-23 22:02:33 -0300

    finish setup project

--- src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var userRoute_1 = require("./routes/userRoute");
-require("dotenv/config");
-var app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use('/user', userRoute_1.default);
-exports.default = app;
